901da44309bc292e004eef3e45065d05adaf16de
eddbot <edwardsansome@gmail.com>
2023-02-04 23:26:50 +0000

    intial

---
+++ scripts/content.js
@@ -0,0 +1,33 @@
+let triggered = false
+const triggerWords = ["World of Warcraft", "world of warcraft", "WoW"]
+
+window.addEventListener('load', (e) => {
+    checkContent(document)
+
+    if (triggered) {
+        const proceed = confirm("page potentially contains triggering content, press OK to proceed or Cancel to redirect to your safe site")
+
+        if (!proceed) {
+            window.location.replace("https://www.google.com")
+        }
+    }
+})
+
+const checkContent = (element) => {
+
+    if (element === null) {
+        console.log("we here")
+        return false
+    }
+    const content = element.innerHTML;
+    triggerWords.forEach(word => {
+        const triggering = content?.includes(word);
+
+        if (triggering) {
+            triggered = true
+        }
+
+    });
+
+    [...element.children].forEach(c => checkContent(c))
+}
